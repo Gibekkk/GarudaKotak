651b7021b9f70ced6d752a5b5985c4604702c01c
Gilbert Winardy <148043329+Gibekkk@users.noreply.github.com>
2025-03-27 14:35:06 +0800

    init commit

---
+++ Garuda/offline.js
@@ -0,0 +1,22 @@
+﻿{
+	"version": 1743057151,
+	"fileList": [
+		"data.js",
+		"c2runtime.js",
+		"jquery-3.4.1.min.js",
+		"offlineClient.js",
+		"images/navbutton-sheet0.png",
+		"images/enemy1-sheet0.png",
+		"images/bluegaruda-sheet0.png",
+		"images/greengaruda-sheet0.png",
+		"images/hurtblock-sheet0.png",
+		"images/healblock-sheet0.png",
+		"images/debuffblock-sheet0.png",
+		"icon-16.png",
+		"icon-32.png",
+		"icon-114.png",
+		"icon-128.png",
+		"icon-256.png",
+		"loading-logo.png"
+	]
+}
